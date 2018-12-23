d86c371a16a92abe5d1824ea5ddefd16a82e36bf
milan102 <hi.milan102@gmail.com>
2018-12-22 20:12:25 -0800

    Add comments

--- data/update_database.py
+++ data/update_database.py
@@ -1,7 +1,9 @@
 import os
 
+# Name of the file
 DATABASE_NAME = "db.sqlite3"
 
+# Updates the existing database
 os.system("python3 main.py -p BTC-USD {}".format(DATABASE_NAME))
 os.system("python3 main.py -p ETH-USD {}".format(DATABASE_NAME))
 os.system("python3 main.py -p LTC-USD {}".format(DATABASE_NAME))
